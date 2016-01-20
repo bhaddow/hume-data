4de1d8d3c19bb5c0b14b3d49183b65c1bd945980
Barry Haddow <barry.haddow@gmail.com>
2016-01-20 16:24:39 +0000

    some tree hacking

--- trees.py
+++ trees.py
@@ -6,18 +6,26 @@ from __future__ import print_function
 #
 import argparse
 import logging
+import math
 import pandas
 import pydot
 import sys
 
+#TODO: Use Cluster and randir=LR for ordering
 
 def plot(nodes):
   """Plot an UCCA tree"""
   graph = pydot.Dot(graph_type='graph')
+  text = pydot.Subgraph(rank = 'same')
   for index,node in nodes.iterrows():
+    if isinstance(node.source, str):
+      textnode = pydot.Node(node.source)
+      text.add_node(textnode)
+      graph.add_edge(pydot.Edge(node.node_id, textnode))
     if node.parent == "0": continue
-    edge = pydot.Edge(node.id, node.parent)
+    edge = pydot.Edge(node.node_id, node.parent)
     graph.add_edge(edge)
+  graph.add_subgraph(text)
   return graph
     
 def main():
@@ -25,12 +33,12 @@ def main():
   parser = argparse.ArgumentParser()
   args = parser.parse_args()
 
-  alldata = pandas.read_csv("data.csv", converters={'id': str, 'parent' : str})
+  alldata = pandas.read_csv("nodes.csv", converters={'node_id': str, 'parent' : str})
   
   annot = "cs1"
   sent = 505
   lang = "cs"
-  selected = alldata[(alldata['user']==annot) & (alldata['sent'] == sent) & (alldata['lang'] == lang)]
+  selected = alldata[(alldata['annot_id']==annot) & (alldata['sent_id'] == sent) & (alldata['lang'] == lang)]
   graph = plot(selected)
   graph.write_png("tree.png")
 
