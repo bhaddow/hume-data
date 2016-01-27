89cfa5e37e48b62ad07cd99bf77b936d8ef17a18
Barry Haddow <barry.haddow@gmail.com>
2016-01-27 09:24:27 +0000

    minor correct to readme

--- tree.py
+++ tree.py
@@ -96,7 +96,7 @@ class Plotter:
     return graph
     
 def main():
-  logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
+  logging.basicConfig(format='%(asctime)s %(levelname)s: %(name)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
   parser = argparse.ArgumentParser()
   args = parser.parse_args()
 
