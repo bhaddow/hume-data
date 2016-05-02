bd2386d8b7e1e44d5c68ce7111a95786afdf3809
Lexi Birch <lexi.birch@gmail.com>
2016-05-02 18:48:00 +0100

    Merged these results with the unannotated HUME test sentences

--- scripts/extract_sentences_for_WMT_metrics_task.py
+++ scripts/extract_sentences_for_WMT_metrics_task.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
 
-import sys
+import sys, string
 import pandas
 from  pandas_confusion import ConfusionMatrix
 
@@ -12,6 +12,9 @@ def main():
   nodedata = pandas.read_csv("nodes.csv", converters={'node_id': str, 'parent' : str})
   scores = score(nodedata)
 
+  #scores = {}
+  #lang = {0:1,2:3}
+  #scores['ro'] = lang
   sentdata = pandas.read_csv("sentences.csv", converters={'node_id': str, 'parent' : str})
   extract(sentdata, scores)
 
@@ -39,7 +42,7 @@ def score(nodedata):
 
       
       score = (g + o/2) / a
-      #print ("Lang:", lang, " Sent:", id, " Plain Score: " , score) 
+      print ("Lang:", lang, " Sent:", id, " Plain Score: " , score) 
 
       langDict[id] = score
 
@@ -53,35 +56,67 @@ def score(nodedata):
 def extract(alldata, scores):
   print ("Stats \n\n")
   for lang in ('ro','de','cs','pl'):
+    (listsrc, listsys, listref) = read_sourcefiles(lang)
     data = alldata.loc[alldata["lang"] == lang]
     ids = alldata['sent_id'].unique()
     print ("Lang: " , lang)
     lang_pair = "en-" + lang
 
     fcsv = open(prefix + "." + lang_pair + ".hume.csv", 'w')
-    fcsv.write("sent_id, source, reference, target, score\n")
+    fcsv.write("sent_id\tsource\treference\ttarget\tscore\tsource_debug\tref_debug\ttarget_debug\n")
 
-    for id in sorted(ids):
+    for id in range(0, len(listsrc)):
+    #for id in sorted(ids):
 
-      datasent = data.loc[data["sent_id"] == id]
+      fcsv.write('\"%s \"\t' % (id))
+      fcsv.write('\"%s \"\t' % (listsrc[id].rstrip()))
+      fcsv.write('\"%s \"\t' % (listref[id].rstrip()))
+      fcsv.write('\"%s \"\t' % (listsys[id].rstrip()))
+      if id + 1 in scores[lang].keys():
+        score = scores[lang][id + 1]
+        print ("Found " + str(id + 1) + " score " + str(score))
+      else: 
+        score = ""
+
+      fcsv.write('\"%s \"\t' % (score))
+
+      datasent = data.loc[data["sent_id"] == id + 1]
       found = 0
       for row_index, row in datasent.iterrows():
         found = 1
-        fcsv.write('\"%s \",' % (row.sent_id))
-        fcsv.write('\"%s \",' % (row.source))
-        fcsv.write('\"%s \",' % (row.reference))
-        fcsv.write('\"%s \",' % (row.target))
-        fcsv.write('\"%s\"\n' % (scores[lang][id]))
+        #fcsv.write('\"%s \"\t' % (row.sent_id))
+        fcsv.write('\"%s \"\t' % (row.source))
+        fcsv.write('\"%s \"\t' % (row.reference))
+        fcsv.write('\"%s \"\t' % (row.target))
      
         break
       
-      if found == 0:
-        print ("missing row" + str(id)) 
+      #if found == 0:
+      #  print ("missing row" + str(id)) 
+      fcsv.write('\n')
 
 
     fcsv.close()
 
+def read_sourcefiles(lang):
+  dir = "data/texts/"
+  listsrc = read_file(dir + "source.en")
+  listsys = read_file(dir + "system." + lang)
+  listref = read_file(dir + "reference." + lang)
+  return (listsrc, listsys, listref) 
 
+def read_file(fileName):
+  lines = []
+  try:
+    inFh = open(fileName, "r")
+    lines = inFh.readlines()
+    for line in lines:
+      line = line.strip('\n')
+    inFh.close()
+  except IOError:
+    print ("I/O error " + fileName) 
+
+  return lines
 
 
 if __name__ == "__main__":
