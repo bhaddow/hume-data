9b8978e10ca758dbe2f16678b3baaff1510ada76
Barry Haddow <barry.haddow@gmail.com>
2016-10-14 09:35:30 +0100

    remove omri code

--- scripts/ucca/omri/get_tasks.py
+++
@@ -1,9 +0,0 @@
-import ucca_db
-
-annotators = ["hagits", "henrybrice", "meirayan", "ayeldabay"]
-
-for a in annotators:
-    completed_tasks = ucca_db.get_tasks("/cs/++/phd/omria01/annotator_db_backup/huca_10_4.db", a)
-    for t in completed_tasks:
-        print(a+'\t'+'\t'.join([str(x) for x in t]))
-
--- scripts/ucca/postags.py
+++
@@ -1,8 +0,0 @@
-"""Static module with part-of-speech tag class."""
-
-
-class POSTags:
-    Noun = 'N'
-    Verb = 'V'
-    Modal = 'M'
-    Other = 'O'
