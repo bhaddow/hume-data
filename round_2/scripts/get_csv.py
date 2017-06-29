5e7d998a4a79b15c0a9cb40da617b1e3fac53e48
Barry Haddow <barry.haddow@gmail.com>
2017-06-29 10:43:25 +0100

    paths

--- round_2/scripts/get_csv.py
+++ round_2/scripts/get_csv.py
@@ -41,8 +41,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--inputFile', nargs='+', dest='inFile', help="Input UCCAMT Eval dump files")
     parser.add_argument('-y', '--sysIndex', help = "System index file", default="../data/raw-annotation/sys_index")
-    parser.add_argument('-s', '--sentenceFile',   help="Sentence tsv file", default="sentences.tsv")
-    parser.add_argument('-n', '--nodeFile',  help="Node tsv file", default="nodes.tsv")
+    parser.add_argument('-s', '--sentenceFile',   help="Sentence tsv file", default="../data/processed/sentences.tsv")
+    parser.add_argument('-n', '--nodeFile',  help="Node tsv file", default="../data/processed/nodes.tsv")
     parser.add_argument("-c", "--comma-delimited", action="store_true")
     args = parser.parse_args()
     
