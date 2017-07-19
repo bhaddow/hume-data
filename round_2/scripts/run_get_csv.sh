5616ddc9c69d37f9ba5bcce185369807456689e5
Barry Haddow <barry.haddow@gmail.com>
2017-07-19 11:03:54 +0100

    not needed

--- round_2/scripts/run_get_csv.sh
+++
@@ -1,3 +0,0 @@
-python3 get_csv.py -i ../data/raw-annotation/user_cs_all0.dump ../data/raw-annotation/user_cs_all1.dump ../data/raw-annotation/user_de_all0.dump ../data/raw-annotation/user_de_all1.dump  ../data/raw-annotation/franzi_nmt.dump ../data/raw-annotation/user_pl_all_3.dump ../data/raw-annotation/user_pl_all_4.dump ../data/raw-annotation/user_pl_all_5.dump ../data/raw-annotation/user_ro_all0.dump ../data/raw-annotation/user_ro_all1.dump ../data/raw-annotation/user_ro_all2.dump -y ../data/raw-annotation/sys_index
-
-
